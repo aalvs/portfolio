12e7910410be29f59508af9df13d1fd64e2091fa
aalvs <andrealvesdeoliveira@outlook.com>
2022-07-22 20:50:04 -0300

    Realizado a referencia dos componentes no App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
+import React from "react";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experince from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experince />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
