fe42a9494c8f7be89876741b472c080aae40846b
aalvs <andrealvesdeoliveira@outlook.com>
2022-07-23 01:11:27 -0300

    Concluída a estilização do componente About.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <div>About</div>;
-};
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me_image">
+            <img src={ME} alt="About Me" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>200+ Worlwide</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet,
+            magni tenetur praesentium minima molestiae numquam nesciunt ab
+            reiciendis quae consequuntur rerum expedita veniam et sequi
+            consectetur omnis. Quaerat, reiciendis minus?
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default About;
