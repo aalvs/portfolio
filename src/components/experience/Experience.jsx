a909514ec8c5e9e681699f3cf29f519a12ce4cd7
aalvs <andrealvesdeoliveira@outlook.com>
2022-07-25 15:01:01 -0300

    Concluído estilização do componente Experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -13,34 +13,60 @@ const Experience = () => {
           <h3>Frontend Developer</h3>
           <div className="experience_content">
             <article className="experience_details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>HTML/CSS</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>Javascript</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>SASS</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>Git</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>Angular.js</h4>
+                <small className="text-light">Proeficient</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>React.js</h4>
+                <small className="text-light">Proeficient</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>Node.js</h4>
+                <small className="text-light">Novice</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>SQL</h4>
+                <small className="text-light">Novice</small>
+              </div>
             </article>
           </div>
         </div>
@@ -48,29 +74,46 @@ const Experience = () => {
           <h3>UX/UI Designer</h3>
           <div className="experience_content">
             <article className="experience_details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>Figma</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>Adobe XD</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>Photoshop</h4>
+                <small className="text-light">Proeficient</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>Illustrator</h4>
+                <small className="text-light">Proeficient</small>
+              </div>
             </article>
             <article className="experience_details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>UX/UI</h4>
+                <small className="text-light">Expert</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon" />
+              <div>
+                <h4>Design System</h4>
+                <small className="text-light">Expert</small>
+              </div>
             </article>
           </div>
         </div>
