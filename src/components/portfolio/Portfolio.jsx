5331daa262d461af61281d5c4e809b71cbff5eee
aalvs <andrealvesdeoliveira@outlook.com>
2022-07-25 17:51:33 -0300

    Exibindo itens do portfolio de forma dinamica

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,49 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/portfolio1.jpg";
-import IMG2 from "../../assets/portfolio2.jpg";
-import IMG3 from "../../assets/portfolio3.jpg";
-import IMG4 from "../../assets/portfolio4.jpg";
-import IMG5 from "../../assets/portfolio5.png";
-import IMG6 from "../../assets/portfolio6.jpg";
+
+const data = [
+  {
+    id: 1,
+    image:
+      "https://cdn.dribbble.com/users/4206856/screenshots/18769569/media/85d3f27f1430b02172ebd4f68e2019e5.png",
+    title: "UI Calculator",
+    github: "https://github.com/aalvs",
+    preview: "https://dribbble.com/shots/18769569-UI-Calculator",
+  },
+  {
+    id: 2,
+    image:
+      "https://cdn.dribbble.com/users/4206856/screenshots/16429466/media/07680c2318e9a519e174955d0bfaac13.png",
+    title: "VOY - SmartPOS",
+    github: "https://github.com/aalvs",
+    preview: "https://dribbble.com/shots/16429466-Voy-Tickets-App-SmartPOS",
+  },
+  {
+    id: 3,
+    image:
+      "https://cdn.dribbble.com/users/4206856/screenshots/16429439/media/15e5c0c82b7a7dca324e96fa60595a60.png",
+    title: "Medical Devices - Marketplace",
+    github: "https://github.com/aalvs",
+    preview: "https://dribbble.com/shots/16429439-Medical-Devices-Marketplace",
+  },
+  {
+    id: 4,
+    image:
+      "https://cdn.dribbble.com/users/4206856/screenshots/16418295/media/a6cbedea86edde9a480eaa114f4294fb.png",
+    title: "DigitalCred - Landing Page",
+    github: "https://github.com/aalvs",
+    preview: "https://dribbble.com/shots/16418295-DigitalCred-Landing-page",
+  },
+  {
+    id: 5,
+    image:
+      "https://cdn.dribbble.com/users/4206856/screenshots/16418281/media/ca16a7406e08844b9b47f8fb26a91fd8.png",
+    title: "DigitalCred -  Funnel Aquisition",
+    github: "https://github.com/aalvs",
+    preview:
+      "https://dribbble.com/shots/16418281-DigitalCred-Real-estate-credit-simulation-form",
+  },
+];
 
 const Portfolio = () => {
   return (
@@ -14,155 +52,34 @@ const Portfolio = () => {
       <h2>Potfolio</h2>
 
       <div className="container portfolio_container">
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG1} alt="This is a portfolio item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta">
-            <a
-              href="https://github.com/aalvs"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://github.com/aalvs"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Preview
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG2} alt="This is a portfolio item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta">
-            <a
-              href="https://github.com/aalvs"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://github.com/aalvs"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Preview
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG3} alt="This is a portfolio item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta">
-            <a
-              href="https://github.com/aalvs"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://github.com/aalvs"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Preview
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG4} alt="This is a portfolio item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta">
-            <a
-              href="https://github.com/aalvs"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://github.com/aalvs"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Preview
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG5} alt="This is a portfolio item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta">
-            <a
-              href="https://github.com/aalvs"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://github.com/aalvs"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Preview
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio_item">
-          <div className="portfolio_item_image">
-            <img src={IMG6} alt="This is a portfolio item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio_item_cta">
-            <a
-              href="https://github.com/aalvs"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://github.com/aalvs"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Preview
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, preview }) => {
+          return (
+            <article className="portfolio_item">
+              <div className="portfolio_item_image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio_item_cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <a
+                  href={preview}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Preview
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
