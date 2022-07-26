dae072a9bbbd567de4fa06de0654b1d4ef6ed5a8
aalvs <andrealvesdeoliveira@outlook.com>
2022-07-26 20:15:06 -0300

    Concluído estilização do componente Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,80 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail className="contact_option_icon" />
+            <h4>Email</h4>
+            <h5>andrealvesdeoliveira@outlook.com</h5>
+            <a
+              href="mailto:andrealvesdeoliveira@outlook.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact_option">
+            <RiMessengerLine className="contact_option_icon" />
+            <h4>Messenger</h4>
+            <h5>aalvs.dev</h5>
+            <a href="https://m.me/aalvs.dev" target="_blank" rel="noreferrer">
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact_option">
+            <BsWhatsapp className="contact_option_icon" />
+            <h4>Whatsapp</h4>
+            <h5>+55 41 99779-1016</h5>
+            <a
+              href="https://wa.me/5541997791016"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            required
+          />
+
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Message"
+            required
+          ></textarea>
+
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
