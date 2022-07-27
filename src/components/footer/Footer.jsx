694065466ffca94918d4a21863ef5a2514bbd3b2
aalvs <andrealvesdeoliveira@outlook.com>
2022-07-26 21:16:26 -0300

    Concluído estilização do componente footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,64 @@
 import React from "react";
 import "./footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        André Alves
+      </a>
+      <ul className="footer_links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer_socials">
+        <a
+          href="https://www.linkedin.com/in/aalvs/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/aalvs" target="_blank" rel="noreferrer">
+          <BsGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/aalvds/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsInstagram />
+        </a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; André Alves. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
